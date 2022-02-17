1f4e5ccccec8435f0ca67cd4bc6af62471a9580c
haraldreingruber-dedalus <harald.reingruber@dedalus.com>
2022-02-17 14:21:24 +0000

    Add screenshot test

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -11,4 +11,8 @@ test.describe('Angular Starter', () => {
     ]);
   });
 
+  test('should have terminal example that matches the screenshot', async ({ page }) => {
+    await expect(await page.locator('.terminal').screenshot()).toMatchSnapshot('terminal.png');
+  });
+
 });
